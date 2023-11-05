8563a7d4a6fc4f62686af11c3f57529042e90e67
igoradm <igorpetrov42ru@gmail.com>
2023-11-05 23:59:13 +0300

    Еще одна неудачная но неплохая попытка, я уже ближе к решению

--- main.asm
+++ main.asm
@@ -2,6 +2,8 @@ format ELF64
 public _start
 
 _start:
-
+    call exit
 exit:
-    mov, 1 ; 1 - вфход из программы - exit
+    mov rax, 1 ; 1 - выход из программы - exit
+    mov rbx, 0 ; 0 - return завершение без ошибки 
+    int 0x80
